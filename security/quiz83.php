b252cf975b52bc6c194b25b784f862654d4e07ac
Onésimo Batista <onesimobatista@gmail.com>
2016-02-29 01:19:12 -0300

    end of security first cicle

---
+++ security/quiz83.php
@@ -0,0 +1,20 @@
+<?php  
+/*
+Where does PHP store uploaded files by default?
+*/
+
+
+
+/*
+
+A In the system's temporary file folder //correct
+
+B In the current script's folder
+
+C In the folder upload_tmp_fir
+
+D im memory
+
+*/
+
+?>
