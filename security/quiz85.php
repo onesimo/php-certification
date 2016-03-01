88e3641af1dc64fd58a79dd0642c5b6a6cdd8f69
Onesimo <onesimobatista@gmail.com>
2016-03-01 11:39:59 -0300

    update gitignore

--- security/quiz85.php
+++ security/quiz85.php
@@ -1,24 +1,8 @@
 <?php  
 /*
 Complete the following statements to make it true in comparison to HTTP connections, HTTPs connections are ...?
-*/
-<<<<<<< HEAD
-<<<<<<< HEAD
  
-
-/* 
-=======
-=======
->>>>>>> b252cf975b52bc6c194b25b784f862654d4e07ac
-
-
-
-/*
-
-<<<<<<< HEAD
->>>>>>> origin
-=======
->>>>>>> b252cf975b52bc6c194b25b784f862654d4e07ac
+ 
 A more secure and better performing
 
 B less secure and slower
