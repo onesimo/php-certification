7ff3ee4b44f3175f07ee8464aa60499d5887b1be
Onesimo <onesimobatista@gmail.com>
2016-03-02 18:54:02 -0300

    second cycle

---
+++ php-basics/basics_second_cycle.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+Syntax 
+
+PHP's sytanx derived from C.
+
+Standar Tags
+<?php ... ?>
+
+Echo Tags - print the result
+<?= $variableEchoed; ?>
+
+Sort Tags
+<? ... ?>
+
+Script Tags
+<script language="php"> </script>
+
+ASP Tags
+<% ... ...  %> 
+//
+
+*/
