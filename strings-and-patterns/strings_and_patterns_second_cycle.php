1ffe3da36e4b2f6c96e5bc8a3a46de15736a70ba
Onésimo Batista <onesimobatista@gmail.com>
2016-03-09 21:24:27 -0300

    update strings

--- strings-and-patterns/strings_and_patterns_second_cycle.php
+++ strings-and-patterns/strings_and_patterns_second_cycle.php
@@ -4,6 +4,15 @@ Strings and Patterns
 
 \ blackslash to escape
 
+echo "\x2a"; //asterix hex
+echo "\052"; //asterix hex
+
+Variable Interpolation
 */
 
-echo "\x2a";
+$me = "Joao";
+$names = array('Maria','Jose', 'Paulo');
+
+echo "There cannot be more than two {$me}s".PHP_EOL;
+
+echo "Citation: {$names[1]}[1997]";
