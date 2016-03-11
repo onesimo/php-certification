15ea9fa18d83f10f3da4a4bfd10049be3edf5451
Onésimo Batista <onesimobatista@gmail.com>
2016-03-11 00:04:42 -0300

    update strings

--- strings-and-patterns/strings_and_patterns_second_cycle.php
+++ strings-and-patterns/strings_and_patterns_second_cycle.php
@@ -138,3 +138,161 @@ strrpos("aaa123123", '123'); // outputs 6
 
 
 //Matching Against mask
+
+$string = '1l3b3445abcdef';
+
+
+//returns the initial mask of allowed characters - whitelist
+strspn($string, '12345');
+
+//
+//returns the length of the initial segment of the string that does not contain any of the characters from mask - blacklist
+strcspn($string, 'yf');
+
+//strcspn and strspn, have two optinal parameters that define the starting position and the lenght of the string
+$string = '1abc234';
+
+strcspn($string, 'c',0,10);
+
+//Simple Search and Replace Operations - case sentive
+str_replace("ola", "oi", "ola pessoas");
+
+//case-insensitive
+str_ireplace("Ola", "Oi", 'ola mundo');
+
+// the third parameter return the number of substitutions made.
+$count= 0;
+str_replace("a", "x", "apapap", $count); // $count = 3
+
+
+str_replace(array("ola", "mundo"), 
+		    array("hello", "world"), "ola mundo"); //hello world
+
+str_replace(array("ola", "mundo"), "tchau", "ola mundo"); // tchau tchau
+
+//third argument is starting point 
+substr_replace("hello world", "reader", 6); // hello world
+
+// the fourth parameters define the end of the substring
+substr_replace("canned tomatoes are good", "corns", 7, 8);
+
+//powerfull tool
+$user = "jhon@email.com";
+$name = substr_replace($user, "", strpos($user, '@')); //jhon
+
+
+//Extracting substrings
+
+$string = "1234567";
+
+//first paramater - starting, if negative start from the end
+
+substr($string, 1); //234567
+substr($string, 0, 3); // 123
+substr($string, -1); //7
+substr($string, -3, 2); //56
+substr($string, 2, 2); // 34
+
+//FORMATTING NUMBERS
+
+number_format('100000.698'); //100,001 nearest integer
+
+//accepts 1,2 or 4 args but not 3
+//second paramater number of decimal, third separates decimal, fourth thousand  
+number_format("100000.698",2, ",", ".");//100.000,69
+
+//Formatting Currency Values
+
+setlocale(LC_MONETARY, "en_US");
+
+//money_format('%.2n',"100000.698"); // this is not work on windows
+
+//Generic Formatting
+
+/*
+b - integer as binary number
+c - character as its ASCII
+d - signed decimal number
+e - scientific notation
+u - unsigned decimal number
+f - locale aware float number
+F - non-locale aware float number
+o - octal representation
+s - string
+x - number as hexadecimal with lowercase letters
+X - number as hexadecimal with uppercase letters
+
+printf()  = writes it to script
+sprintf() = return it
+fprinf()  = print it out to file
+ 
+Using printf
+*/
+
+
+
+
+$n = 123;
+$f = 123.45;
+$s = 'A String';
+
+//printf("%d", $n); // prints 123
+//printf("%f", $f); // prints 123.450000
+
+//printf("the string is a %s", $s);
+
+//printf("%3.3f", $f); // 123.450
+
+//complex formating
+
+function showError($msg, $line, $file){
+	return sprintf("An error occured in %s on line %d: %s", $file, $line, $msg);
+}
+showError("invalid number", __LINE__, __FILE__);
+
+/*
+Parsing formart input 
+sscanf() similiar to prinf() - instead of formatting output, allows to parse formatted input.
+*/
+
+$data = '123 456 456';
+$format = '%d %d %d';
+
+$array = (sscanf($data, $format));
+ 
+
+//the function interprets the format string and returns an array
+
+
+/*
+PERL COMPATIBLE REGULAR EXPRESSIONS
+
+Demiliter
+
+by convention slash(/)
+
+Metacharacters
+
+. Match any character
+^ Match the start of the string
+$ Match the end of the string
+\s Match any whitespace character
+\d Match any digit
+\w Match any "word" character
+
+using grouping expression
+
+/ab[cd]e/  can be match  abce and acde 
+
+provide range
+
+/ab[c-e\d]/ can be match abc, abd, abe and any combination of ab followed by a digit
+
+Quantifiers
+
+* zero or more times
++ apper one or more times
+? zero or one time
+{n,m} at least n times and no more than m
+
+*/
