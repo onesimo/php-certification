5c4c22cedecef2482548d778f655f28091248b5e
Onésimo Batista <onesimobatista@gmail.com>
2016-04-05 00:06:43 -0300

    update closure

--- object-oriented-programming/closure.php
+++ object-oriented-programming/closure.php
@@ -5,7 +5,7 @@
 */
 
 $closure = function($who) {
-	echo "Hello $who";
+	//echo "Hello $who";
 };
 
 $closure("world");
@@ -13,18 +13,57 @@ $closure("world");
 Scope
 
 Creating a closure
-*/
+
 
 function createGreeter($who){
 	return function () use ($who){
-		echo "hello $who";
+		//echo "hello $who";
 	};
 }
 
 $greeter = createGreeter("world");
 $greeter();
+*/
+/**
+* Creating a closure with a reference
+*
+*/
 
 
+//make sure to use referecen here also
+function createGreeter(&$who){
+	return function () use (&$who){
+		echo "hello $who";
+		$who = null;
+	};
+}
 
+$who = 'world';
+$greeter = createGreeter($who);
+$who = ucfirst($who);
+
+$greeter();
+
+var_dump($who);
+
+/*
+
+Using $this
+*/
+
+class foo{
+	public function getClosure(){
+		return function () { return $this; };
+	}
+}
+
+class bar{
+	public function __construct(){
+		$foo = new foo();
+		$func = $foo->getClosure();
+		$obj = $func(); // PHP 5.3: $obj == null
+						// PHP 5.4: $obj == foo, not bar 
+	}
+}
 
 ?>
