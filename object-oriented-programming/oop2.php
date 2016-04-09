0b41d1fd322ae643d26064de20d1a924dc66b8fb
Onésimo Batista <onesimobatista@gmail.com>
2016-04-09 00:12:04 -0300

    update oop

--- object-oriented-programming/oop2.php
+++ object-oriented-programming/oop2.php
@@ -3,4 +3,27 @@
 * Elements of Object-Oriented Design
 *
 *
-*/
+*
+
+Traits */
+
+namespace Ds\Util;
+
+
+trait SecureDebugInfo{
+	public function __debugInfo(){
+		$info = (array) $this;
+
+		foreach ($info as $key => $value) {
+			if($key[0] == '_'){
+				unset($info[$key]);
+			}
+		}
+	}
+}
+
+namespace Ds;
+
+class Myclass{
+	use Util\SecureDebugInfo;
+}
