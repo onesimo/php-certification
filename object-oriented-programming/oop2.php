3b10a4b1db2725690e18f2878153df15aa0d4767
Onésimo Batista <onesimobatista@gmail.com>
2016-04-10 21:41:43 -0300

    update oop

--- object-oriented-programming/oop2.php
+++ object-oriented-programming/oop2.php
@@ -28,3 +28,53 @@ class Myclass{
 	use Util\SecureDebugInfo;
 }
 
+
+trait One{
+	public function one(){}
+}
+
+trait Two{
+	public function two(){}
+}
+
+trait Three{
+	use One, Two; // trait three now comprise of all three traits.
+	public function Three(){}
+}
+
+/*
+Trait inheritance
+*/
+
+class Numbers{
+	use One; // adds One->one();
+
+	public function two(){}
+	public function three(){}
+}
+
+class MoreNumbers extends Numbers
+{
+	use Two; //Two->two() overrides  Numbers->two()
+
+	public function one(){ } // Overrides Number->one()
+	//
+}
+
+class EvenMoreNumbers extends MoreNumbers{
+	use Three;
+	/*
+	Three->one() Overrides MoreNumbers->one()
+	Three->two() Overrides MoreNumbers->two()
+
+	Three->three() Overrides inherited Numbers->three()
+	*/
+
+	public function three(){} // Overrides Three->three()
+}
+
+/*
+Trait Requirements
+
+
+*/
