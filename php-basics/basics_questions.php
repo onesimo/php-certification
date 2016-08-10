62ac2a954204f9a229ff0754705f393062855235
Onésimo Batista <onesimobatista@gmail.com>
2016-08-09 21:47:31 -0300

    update ...

--- php-basics/basics_questions.php
+++ php-basics/basics_questions.php
@@ -100,5 +100,13 @@ $array = array (1, 2, 3);
 modifyArray($array);
 print_r($array);
 
+Which of the below provided options is correct regarding the below code? 
 
 */
+
+$a = array (1, 2, 3);
+$b = array (1 => 2, 2 => 3, 0 => 1); 
+$c = array ('a' => 1, 'b' => 2, 'c' => 3);
+var_dump ($a == $b); 
+var_dump ($a === $b); 
+var_dump ($a == $c); 
